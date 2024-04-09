0b2a90fdb80a22ea3a477b838ae3022a61a3e34a
DungLe2983 <lequocdung2983@gmail.com>
2024-04-10 04:07:51 +0700

    feat: create ContactPage

--- app/components/menu/ProductSection.js
+++ app/components/menu/ProductSection.js
@@ -1,118 +1,94 @@
 import Image from 'next/image';
 import React from 'react';
-const filteredProducts = [
+import ProductCard from './ProductCard';
+const filterProducts = [
     {
-        id: 1,
-        title: 'Product 1',
-        desc: 'Description of Product 1',
-        price: 10.99,
-        image: '/productImg.jpg',
-        rate: 4,
+        image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrai11_19.jpg',
+        hoverImage:
+            'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang4_22.jpg',
+        name: 'Áo thun Cotton Compact In Lụa VỤN ART Logo Trái V1',
+        subtitle: '100% Cotton / Đen',
+        price: '399.000đ',
+        rate: '5',
     },
     {
-        id: 2,
-        title: 'Product 2',
-        desc: 'Description of Product 2',
-        price: 19.99,
-        image: '/productImg.jpg',
-        rate: 4.5,
+        image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
+        name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
+        hoverImage:
+            'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
+        subtitle: 'Care & Share / Đen Trắng',
+        price: '399.000đ',
+        rate: '5',
     },
     {
-        id: 3,
-        title: 'Product 3',
-        desc: 'Description of Product 3',
-        price: 8.99,
-        image: '/productImg.jpg',
-        rate: 5,
+        image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
+        hoverImage:
+            'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
+        name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
+        subtitle: 'Care & Share / Trắng',
+        price: '299.000đ',
+        rate: '5',
     },
     {
-        id: 4,
-        title: 'Product 4',
-        desc: 'Description of Product 4',
-        price: 12.99,
-        image: '/productImg.jpg',
-        rate: 3.8,
+        image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrai11_19.jpg',
+        hoverImage:
+            'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang4_22.jpg',
+        name: 'Áo thun Cotton Compact In Lụa VỤN ART Logo Trái V1',
+        subtitle: '100% Cotton / Đen',
+        price: '399.000đ',
+        rate: '5',
     },
     {
-        id: 5,
-        title: 'Product 5',
-        desc: 'Description of Product 5',
-        price: 15.99,
-        image: '/productImg.jpg',
-        rate: 5,
+        image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
+        name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
+        hoverImage:
+            'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
+        subtitle: 'Care & Share / Đen Trắng',
+        price: '399.000đ',
+        rate: '5',
     },
     {
-        id: 6,
-        title: 'Product 6',
-        desc: 'Description of Product 6',
-        price: 9.99,
-        image: '/productImg.jpg',
-        rate: 5,
+        image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
+        hoverImage:
+            'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
+        name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
+        subtitle: 'Care & Share / Trắng',
+        price: '299.000đ',
+        rate: '5',
     },
     {
-        id: 7,
-        title: 'Product 7',
-        desc: 'Description of Produ aaa',
-        price: 14.99,
-        image: '/productImg.jpg',
-        rate: 5,
+        image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
+        name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
+        hoverImage:
+            'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
+        subtitle: 'Care & Share / Đen Trắng',
+        price: '399.000đ',
+        rate: '5',
     },
     {
-        id: 8,
-        title: 'Product 8',
-        desc: 'Description of Product 8',
-        price: 11.99,
-        image: '/productImg.jpg',
-        rate: 5,
+        image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
+        hoverImage:
+            'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
+        name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
+        subtitle: 'Care & Share / Trắng',
+        price: '299.000đ',
+        rate: '5',
     },
-    {
-        id: 9,
-        title: 'Product 9',
-        desc: 'Description of Product 9',
-        price: 13.99,
-        image: '/productImg.jpg',
-        rate: 5,
-    },
-    {
-        id: 10,
-        title: 'Product 10',
-        desc: 'Description of Product 10',
-        price: 7.99,
-        image: '/productImg.jpg',
-        rate: 5,
-    },
+    //...
 ];
 const ProductSection = () => {
     return (
         <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-12'>
-            {filteredProducts.map((product) => (
-                <div key={product.id}>
-                    <Image
-                        src={product.image}
-                        alt={product.title}
-                        width={400}
-                        height={400}
-                        className='rounded-md shadow-sm  aspect-[4/5] object-cover object-top cursor-pointer'
-                    />
-                    <div className='space-y-1'>
-                        <div>
-                            <p className='mt-4 font-medium truncate cursor-pointer'>
-                                {' '}
-                                {product.title}
-                            </p>
-                        </div>
-                        <p className=' line-clamp-2 text-slate-500 text-sm '>
-                            {product.desc}
-                        </p>
-                        <div className='flex justify-between items-center'>
-                            <p className='font-semibold'>Rs: {product.price}</p>
-                            <p className='flex gap-2 items-center mr-2 text-sm'>
-                                <i className='ri-star-fill text-yellow-400'></i>
-                                <span className='font-semibold'>{product.rate}</span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+            {filterProducts.map((product, index) => (
+                <ProductCard
+                    key={index}
+                    image={product.image}
+                    hoverImage={product.hoverImage}
+                    name={product.name}
+                    subtitle={product.subtitle}
+                    price={product.price}
+                    rate={product.rate}
+                />
             ))}
             ;
         </div>
---
+++ app/contact/page.js
@@ -0,0 +1,128 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <div className='font-sans text-base text-gray-900 sm:px-10'>
+                <div className='text-base text-gray-900'>
+                    <div className='mx-auto w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl'>
+                        <div className='mx-2 pt-12 text-center md:mx-auto md:w-2/3 md:pb-12'>
+                            <h2 className='mb-4 text-xxl font-black sm:text-3xl xl:text-4xl'>
+                                Liên hệ với Chúng tôi
+                            </h2>
+                            <div className='text-sm sm:text-base xl:text-lg'>
+                                <div className='text-gray-900'>
+                                    <p className='mb-4'>
+                                        Đừng ngần ngại, chúng tôi mong muốn nghe
+                                        từ bạn! Hãy đặt câu hỏi, chia sẻ ý kiến
+                                        hoặc yêu cầu của bạn ngay bây giờ. Chúng
+                                        tôi luôn sẵn lòng hỗ trợ bạn!
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='mx-auto mb-20 flex w-full max-w-screen-lg flex-col overflow-hidden rounded-xl text-gray-900 md:flex-row md:border md:shadow-lg'>
+                    <form className='mx-auto w-full max-w-xl border-gray-200 px-10 py-8 md:px-8'>
+                        <div className='mb-4'>
+                            <label
+                                className='text mb-2 block font-medium'
+                                for='email'
+                            >
+                                E-mail:
+                            </label>
+                            <input
+                                className='bg-gray-50 border border-gray-300
+                            text-gray-900 sm:text-sm rounded
+                            focus:ring-gray-500 focus:border-gray-500 block
+                            w-full p-2.5 '
+                                id='email'
+                                type='email'
+                                required=''
+                            />
+                        </div>
+                        <div className='mb-4'>
+                            <label
+                                className='text mb-2 block font-medium'
+                                for='subject'
+                            >
+                                Tiêu đề:
+                            </label>
+                            <input
+                                className='bg-gray-50 border border-gray-300
+                            text-gray-900 sm:text-sm rounded
+                            focus:ring-gray-500 focus:border-gray-500 block
+                            w-full p-2.5 '
+                                id='subject'
+                                type='subject'
+                                required=''
+                            />
+                        </div>
+                        <div className='mb-4'>
+                            <label
+                                className='text mb-2 block font-medium'
+                                for='message'
+                            >
+                                Nội dung:
+                            </label>
+                            <textarea
+                                className='bg-gray-50 h-52 border border-gray-300
+                            text-gray-900 sm:text-sm rounded
+                            focus:ring-gray-500 focus:border-gray-500 block
+                            w-full p-2.5 '
+                                id='message'
+                                required=''
+                            ></textarea>
+                        </div>
+                        <div className='flex items-center'>
+                            <div className='flex-1'></div>
+                            <button
+                                className='flex items-center gap-2 rounded-lg bg-primary px-6 py-2 text-center font-bold text-white '
+                                type='submit'
+                            >
+                                <i className='ri-send-plane-fill'></i>
+                                <p className='text-base'>Gửi</p>
+                            </button>
+                        </div>
+                    </form>
+                    <div className='mt-10 bg-primary px-10 py-8 text-gray-100 md:mt-0 md:ml-auto'>
+                        <div className=''>
+                            <p className='mb-4 font-medium border-b  pb-2'>
+                                GIỜ LÀM VIỆC
+                            </p>
+                            <p className='mb-4'>Thứ 3 – Thứ 7: 08:00 – 20:00</p>
+                            <p className='mb-4'>Chủ Nhật: 08:00 - 22:00</p>
+                            <p className='mb-4'>Thứ 2: Closed</p>
+                            <p className='mb-4'>
+                                Email:
+                                <a href='#' className='font-semibold underline'>
+                                    {' '}
+                                    supportFashionFusion@gmail.com
+                                </a>
+                            </p>
+                            <p className='mb-4'>
+                                Phone:
+                                <a href='#' className='font-semibold underline'>
+                                    {' '}
+                                    +84 (0) 10-32 32 322
+                                </a>
+                            </p>
+                            <hr className='my-2 h-0 border-t border-r-0 border-b-0 border-l-0 border-gray-300' />
+                            <p className='mb-4'>Org.no: 63452-2832</p>
+                            <p className='mb-4'>VAT no: 32353</p>
+                            <hr className='my-4 h-0 border-t border-r-0 border-b-0 border-l-0 border-gray-300' />
+                            <p>Địa chỉ:</p>
+                            <p>
+                                Tầng 17 Saigon Centre 2, 67 Lê Lợi, Phường Bến
+                                Nghé, Quận 1, TP. Hồ Chí Minh
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactPage;
