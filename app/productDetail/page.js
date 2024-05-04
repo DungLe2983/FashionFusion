dbb5e80c1ea7dfc76fc13e7d5142534ba3be7f96
DungLe2983 <lequocdung2983@gmail.com>
2024-05-04 14:20:56 +0700

    feat: create RelatedProduct card

--- app/productDetail/page.js
+++ app/productDetail/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import ProductCard from '../components/menu/ProductCard';
+import RelatedProduct from '../components/layout/RelatedProduct';
 
 const ProductDetailPage = () => {
     const [selectedSize, setSelectedSize] = useState(null);
@@ -31,82 +32,6 @@ const ProductDetailPage = () => {
             description: '3XL(1m84 - 1m88 | 83kg - 87kg)',
         },
     ];
-    const products = [
-        {
-            image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrai11_19.jpg',
-            hoverImage:
-                'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang4_22.jpg',
-            name: 'Áo thun Cotton Compact In Lụa VỤN ART Logo Trái V1',
-            subtitle: '100% Cotton / Đen',
-            price: '399.000đ',
-            rate: '5',
-        },
-        {
-            image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
-            name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
-            hoverImage:
-                'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
-            subtitle: 'Care & Share / Đen Trắng',
-            price: '399.000đ',
-            rate: '4',
-        },
-        {
-            image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
-            hoverImage:
-                'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
-            name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
-            subtitle: 'Care & Share / Trắng',
-            price: '299.000đ',
-            rate: '4',
-        },
-        {
-            image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrai11_19.jpg',
-            hoverImage:
-                'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang4_22.jpg',
-            name: 'Áo thun Cotton Compact In Lụa VỤN ART Logo Trái V1',
-            subtitle: '100% Cotton / Đen',
-            price: '399.000đ',
-            rate: '4',
-        },
-        {
-            image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
-            name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
-            hoverImage:
-                'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
-            subtitle: 'Care & Share / Đen Trắng',
-            price: '399.000đ',
-            rate: '4',
-        },
-        {
-            image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
-            hoverImage:
-                'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
-            name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
-            subtitle: 'Care & Share / Trắng',
-            price: '299.000đ',
-            rate: '4',
-        },
-        {
-            image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
-            name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
-            hoverImage:
-                'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
-            subtitle: 'Care & Share / Đen Trắng',
-            price: '399.000đ',
-            rate: '4',
-        },
-        {
-            image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
-            hoverImage:
-                'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
-            name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
-            subtitle: 'Care & Share / Trắng',
-            price: '299.000đ',
-            rate: '4',
-        },
-        //...
-    ];
-
     const handleSizeClick = (size) => {
         setSelectedSize(size);
     };
@@ -120,11 +45,7 @@ const ProductDetailPage = () => {
                             src='https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrai11_19.jpg'
                             alt='img'
                         />
-                        {/* <img
-                            className='max-w-[screen] md:max-w-sm lg:max-w-xl '
-                            src='/productImg.jpg'
-                            alt='img'
-                        /> */}
+
                     </div>
                     <div className='text-left '>
                         <h2 className='text-xl md:text-2xl font-medium mb-2  max-w-[450px] mx-auto lg:mx-0'>
@@ -209,24 +130,7 @@ const ProductDetailPage = () => {
                     </div>
                 </div>
             </div>
-            <div className='mt-16'>
-                <h2 className='text-2xl my-10 text-primary text-center font-semibold '>
-                    Các sản phẩm tương tự:
-                </h2>
-                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 md:gap-x-[20px] gap-y-10 p-4 '>
-                    {products.map((product, index) => (
-                        <ProductCard
-                            key={index}
-                            image={product.image}
-                            hoverImage={product.hoverImage}
-                            name={product.name}
-                            subtitle={product.subtitle}
-                            price={product.price}
-                            rate={product.rate}
-                        />
-                    ))}
-                </div>
-            </div>
+            <RelatedProduct/>
         </section>
     );
 };
