fb5b8d8980c744932dbd2d17abf934c61971a12b
DungLe2983 <lequocdung2983@gmail.com>
2024-05-04 14:20:56 +0700

    feat: create RelatedProduct card

---
+++ app/components/layout/RelatedProduct.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import ProductCard from '../menu/ProductCard';
+const RelatedProducts = [
+     {
+         image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrai11_19.jpg',
+         hoverImage:
+             'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang4_22.jpg',
+         name: 'Áo thun Cotton Compact In Lụa VỤN ART Logo Trái V1',
+         subtitle: '100% Cotton / Đen',
+         price: '399.000đ',
+         rate: '5',
+     },
+     {
+         image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
+         name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
+         hoverImage:
+             'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
+         subtitle: 'Care & Share / Đen Trắng',
+         price: '399.000đ',
+         rate: '4',
+     },
+     {
+         image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
+         hoverImage:
+             'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
+         name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
+         subtitle: 'Care & Share / Trắng',
+         price: '299.000đ',
+         rate: '4',
+     },
+     {
+         image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrai11_19.jpg',
+         hoverImage:
+             'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang4_22.jpg',
+         name: 'Áo thun Cotton Compact In Lụa VỤN ART Logo Trái V1',
+         subtitle: '100% Cotton / Đen',
+         price: '399.000đ',
+         rate: '4',
+     },
+     {
+         image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
+         name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
+         hoverImage:
+             'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
+         subtitle: 'Care & Share / Đen Trắng',
+         price: '399.000đ',
+         rate: '4',
+     },
+     {
+         image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
+         hoverImage:
+             'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
+         name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
+         subtitle: 'Care & Share / Trắng',
+         price: '299.000đ',
+         rate: '4',
+     },
+     {
+         image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunartqyay2_72.jpg',
+         name: 'Áo thun Cotton Compact In Lụa VỤN ART Chơi Quay',
+         hoverImage:
+             'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang7_23.jpg',
+         subtitle: 'Care & Share / Đen Trắng',
+         price: '399.000đ',
+         rate: '4',
+     },
+     {
+         image: 'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/oantuti1_2.jpg',
+         hoverImage:
+             'https://mcdn2.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/vunarttrang3_40.jpg',
+         name: 'Áo thun Cotton Compact In Lụa VỤN ART Oẳn Tù Tì',
+         subtitle: 'Care & Share / Trắng',
+         price: '299.000đ',
+         rate: '4',
+     },
+     //...
+ ];
+const RelatedProduct = () => {
+
+  return (
+      <div className='mt-16'>
+          <h2 className='text-2xl my-10 text-primary text-center font-semibold '>
+              Các sản phẩm tương tự:
+          </h2>
+          <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-4 md:gap-x-[20px] gap-y-10 p-4 '>
+              {RelatedProducts.slice(0,8).map((product, index) => (
+                  <ProductCard
+                      key={index}
+                      image={product.image}
+                      hoverImage={product.hoverImage}
+                      name={product.name}
+                      subtitle={product.subtitle}
+                      price={product.price}
+                      rate={product.rate}
+                  />
+              ))}
+          </div>
+      </div>
+  );
+}
+
+export default RelatedProduct
