478f123304cdb7f15fdbac1a5ba6775dd5ad1995
DungLe2983 <lequocdung2983@gmail.com>
2024-06-20 13:13:14 +0700

    fix: update Pagination feature

--- app/products/page.js
+++ app/products/page.js
@@ -1,8 +1,43 @@
+'use client';
+import { useEffect, useState } from 'react';
 import FilterSection from '../components/layout/FilterSection';
 import ProductSection from '../components/menu/ProductSection';
 import AppPagination from '../components/Pagination';
 
-export default function ProductPage() {
+const ProductPage = () => {
+    const [products, setProducts] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handlePageChange = (event, newPage) => {
+        setCurrentPage(newPage);
+    };
+
+    async function getProducts() {
+        try {
+            const res = await fetch(`/api/products?page=${currentPage}`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (res.ok) {
+                const data = await res.json();
+                // console.log('data', data);
+                if (data) {
+                    setProducts(data);
+                } else {
+                    console.error('User not found or response is empty.');
+                }
+            } else {
+                console.error('Error fetching user:', res.statusText);
+            }
+        } catch (error) {
+            console.error('Error in fetch:', error);
+        }
+    }
+    useEffect(() => {
+        getProducts();
+    }, [currentPage]);
+
     return (
         <main className='max-w-7xl mx-auto '>
             <img
@@ -16,10 +51,14 @@ export default function ProductPage() {
             <div className='grid grid-cols-8 py-8 gap-10'>
                 <FilterSection />
                 <div className='col-span-6'>
-                    <ProductSection />
-                    <AppPagination />
+                    <ProductSection products={products} />
+                    <AppPagination
+                        currentPage={currentPage}
+                        onPageChange={handlePageChange}
+                    />
                 </div>
             </div>
         </main>
     );
-}
+};
+export default ProductPage;
